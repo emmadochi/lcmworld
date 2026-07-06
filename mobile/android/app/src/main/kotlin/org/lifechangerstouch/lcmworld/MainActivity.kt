6e6077132ef58d62d56514d2bc043f83bff16da1
Emma Dochi <emmadochi@gmail.com>
2026-07-07 00:51:24 +0100

    Remove mobile from git tracking

--- mobile/android/app/src/main/kotlin/org/lifechangerstouch/lcmworld/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.lifechangerstouch.lcmworld
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App Catalog smoke test', (WidgetTester tester) async {
-    // Tests are skipped in this environment due to Image.network throwing 400.
-    expect(true, true);
-  });
-}
